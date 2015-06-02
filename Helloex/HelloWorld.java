e77a373b41c0cd77935a2a64a34b34ab18ff1808
DenDK <denis.hodachuk@gmail.com>
2015-06-02 15:13:29 +0300

    0-helloex

---
+++ Helloex/HelloWorld.java
@@ -0,0 +1,7 @@
+package team10.Helloex;
+import team10.Printer;
+public class HelloWorld{
+    public static void main(String[] args) {
+        Printer.printo();
+    }
+}
---
+++ Helloex/Printer.java
@@ -0,0 +1,7 @@
+package team10.Helloex;
+public class Printer{
+  public static void printo(){
+      System.out.println("Hello World!"); 
+}
+}
+    
